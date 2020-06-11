4d3b304764529676c7df2b962537d37eaf8604d9
edenbulur58 <64384006+edenbulur58@users.noreply.github.com>
2020-06-11 15:40:07 +0200

    commit 8

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -3,7 +3,9 @@ package gitPackage;
 public class NextClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello DEAR friends");
 
 	}
 
