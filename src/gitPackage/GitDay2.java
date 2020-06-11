d56df362a647078c65c89634173b3d2d1437ed97
Fatih Kılıç <edenbulur58@gmail.com>
2020-06-11 08:19:08 +0200

    GitDay2 added

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -2,9 +2,12 @@ package gitPackage;
 
 public class GitDay2 {
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+System.out.println("Hello World!");
 	}
 
 }
