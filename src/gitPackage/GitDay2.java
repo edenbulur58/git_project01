5cca9a9f9cf11cb52a48b13ed8daeeea6db104b8
Fatih Kılıç <edenbulur58@gmail.com>
2020-06-11 09:17:39 +0200

    sixth commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -9,7 +9,9 @@ public class GitDay2 {
 		
 System.out.println("Hello World!");
 System.out.println("Hello Git Users!");
+System.out.println("Check if your local is ahead of origin master!");
 System.out.println("Everything is good!");
+
 	}
 
 }
